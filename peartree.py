5d2b9e7fd79bd65626b506859953293ca5f0dfd7
Jerry Chen <chenhongjin2005@hotmail.com>
2021-11-08 02:06:12 -0500

    generates graph

--- peartree.py
+++
@@ -1,8 +0,0 @@
-import partridge as ptg
-inpath = '/Users/jerry/Developer/EECS4414_ttc_bus_delay/opendata_ttc_schedules.zip'
-service_ids = ptg.read_busiest_date(inpath)[1]
-view = {'trips.txt': {'service_id': service_ids}}
-
-feed = ptg.load_geo_feed(inpath, view)
-
-print(feed.shapes.head())
