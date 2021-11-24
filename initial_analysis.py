948ea3449bdd2db44ed4a8bcb945174df08cc6f5
Lily <lilywuyanru@gmail.com>
2021-11-23 19:16:47 -0500

    calculations

--- initial_analysis.py
+++ initial_analysis.py
@@ -1,6 +1,8 @@
 import networkx as nx
 from itertools import islice
 import matplotlib.pyplot as plt
+import networkx as nx
+import scipy as sp
 
 G = nx.read_gpickle("transit_graph.gpickle")
 G.add_node('abc', dob=1185, pob='usa', dayob='monday')
@@ -17,4 +19,24 @@ plt.ylabel("Count")
 plt.show()
 # for item in islice(sorted_dict.items(), 50): 
 #     print(G.nodes[item[0]]["stop_name"])
+print("with all nodes")
+# print(nx.edge_betweenness_centrality(pagerank))
+# print(nx.global_efficiency(pagerank))
+# print(nx.average_clustering(pagerank))
 
+f = open("calculation.txt")
+
+for k, v in sorted_dict:
+    temp = pagerank.remove_node(k)
+    f.write("remove " + k + " : ")
+    print("remove " + k + " : ")
+    # f.write(nx.edge_betweenness_centrality(temp))
+    # print(nx.edge_betweenness_centrality(temp))
+    f.write(nx.global_efficiency(temp))
+    print(nx.global_efficiency(temp))
+    f.write(nx.average_clustering(temp))
+    print(nx.average_clustering(temp))
+    break
+
+f.close()
+  
