01f1404e90fcec962917a83678be27300ac3db8e
Lily <lilywuyanru@gmail.com>
2021-11-28 11:22:19 -0500

    plot acc graph

--- initial_analysis.py
+++ initial_analysis.py
@@ -25,31 +25,58 @@ print("sorted")
 # print(nx.global_efficiency(pagerank))
 # print(nx.average_clustering(pagerank))
 
-g = open("global_eff_calculation.txt", 'w')
-a = open("average_clustering_calculation.txt", 'w')
+g = open("global_eff_calculation.txt", 'r')
 
-G = G.to_undirected()
-print("here")
+with open('average_clustering_calculation.txt') as f:
+    lines = f.readlines()
 
-# OG graph
-print("global")
-g.write(str(nx.global_efficiency(G))+"\n")
-H = nx.Graph(G)
-print("avg clustering")
-a.write(str(nx.average_clustering(H))+"\n")
-print("done")
+new_lines=[]
+for line in lines:
+    new_lines.append(float(line))
 
-# Remove nodes one by one
-for k, v in sorted_dict.items():
-    print("k: "+ k)
-    G.remove_node(k)
-    print("global")
-    g.write(str(nx.global_efficiency(G))+"\n")
-    H = nx.Graph(G)
-    print("avg clustering")
-    a.write(str(nx.average_clustering(H))+"\n")
-    # break
+acc_data = [
+    sum(new_lines[0:879])/879, 
+    sum(new_lines[880:1758])/879, 
+    sum(new_lines[1759:2637])/879,
+    sum(new_lines[2638:3516])/879,
+    sum(new_lines[3517:4395])/879,
+    sum(new_lines[4396:5274])/879,
+    sum(new_lines[5275:6153])/879,
+    sum(new_lines[6154:7032])/879,
+    sum(new_lines[7033:7911])/879,
+    sum(new_lines[7912:8798])/886,
+]
 
-g.close()
-a.close()
+x_values = ["10%", "20%","30%","40%","50%","60%","70%","80%","90%","100%"]
+plt.scatter(x_values, acc_data)
+plt.plot(x_values, acc_data)
+plt.title("Distribution of average clustering coefficient")
+plt.xlabel("percentage")
+plt.ylabel("average clustering coefficient")
+plt.show()
+
+# G = G.to_undirected()
+# print("here")
+
+# # OG graph
+# print("global")
+# g.write(str(nx.global_efficiency(G))+"\n")
+# H = nx.Graph(G)
+# print("avg clustering")
+# a.write(str(nx.average_clustering(H))+"\n")
+# print("done")
+
+# # Remove nodes one by one
+# for k, v in sorted_dict.items():
+#     print("k: "+ k)
+#     G.remove_node(k)
+#     print("global")
+#     g.write(str(nx.global_efficiency(G))+"\n")
+#     H = nx.Graph(G)
+#     print("avg clustering")
+#     a.write(str(nx.average_clustering(H))+"\n")
+#     # break
+
+# g.close()
+# a.close()
   
